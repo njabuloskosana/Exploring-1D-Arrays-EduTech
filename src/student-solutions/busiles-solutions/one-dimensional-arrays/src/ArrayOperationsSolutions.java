b96f14e9851ced41cb699c47f6519a4b6ea04117
Njabulo <njabulo@xcontent.com>
2022-04-11 11:38:30 +0200

    fixing up students development environment

---
+++ src/student-solutions/busiles-solutions/one-dimensional-arrays/src/ArrayOperationsSolutions.java
@@ -0,0 +1,201 @@
+public class ArrayOperationsSolutions {
+    
+    /**
+     * The method sum will calculate the sum of all values 
+     * in the array and return the sum, if the array is empty return zero
+     * arr=[1,2,3,4] returns 10
+     * @param arr
+     * @return
+     */
+    public int sum(int[]arr)
+    {
+        return 0;
+
+    }
+
+    /**
+     * The method average will calculate the average of all the values 
+     * within the array an return the average if the array is empty
+     * return zero
+     * arr=[1,2,3,4] returns 2,5
+     * @param arr
+     * @return
+     */
+    public double average(int[]arr)
+    {
+        return 0;
+    }
+
+    /**
+     * The method contains returns true if value is found in the 
+     * array and false if the value is not in the array
+     * if the array is empty it returns zero
+     * arr=[6,9,0,4] value=0 retur true
+     * @param arr
+     * @param value
+     * @return
+     */
+    public boolean contains(int[]arr,int value)
+    {
+        return false;
+    }
+
+    /**
+     * This method removes value from the array and returns back 
+     * a new array without the value, if the value is not present
+     * the original array is returned, if the array is empty return 
+     * null
+     * arr=[6,7,8,4,9,0] and value=8 return [6,7,4,9,0] 
+     * @param arr
+     * @param value
+     * @return
+     */
+    public int[] remove(int[]arr,int value)
+    {
+        return null;
+    }
+
+    /**
+     * This method finds the highest value in the array and returns
+     * the value, if the array is empty return zero
+     * arr=[9,7,4,3] return 9
+     * @param arr
+     * @return
+     */
+    public int maximum(int[] arr)
+    {
+        return 0;
+    }
+
+    /**
+     * This method finds the lowest value in the array and returns
+     * the value, if the array is empty return zero 
+     * arr=[5,4,1,9] return 1
+     * @param arr
+     * @return
+     */
+    public int minimum(int[] arr)
+    {
+        return 0;
+    }
+
+    /**
+     * This method find the second lowest value in the array
+     * and returns the value, if the array is empty return 
+     * zero arr=[1,4,3,2] returns 2
+     * @param arr
+     * @return
+     */
+    public int secondSmallest(int[] arr)
+    {
+        return 0;
+    }
+
+    /**
+     * This method returns the reversed version of the array
+     * [1,2,3,4] becomes [4,3,2,1], if the array is empty
+     * return null
+     * @param arr
+     * @return
+     */
+    public int[] reverse(int[] arr)
+    {
+        return null;
+    }
+
+    /**
+     * This method recieves an array with duplicate values and 
+     * returns a new array without duplicates [1,2,2,3,4,4] 
+     * becomes [1,2,3,4], if the array is empty return null
+     * @param arr
+     * @return
+     */
+    public int[] removeDuplicates(int[] arr)
+    {
+        return null;
+    }
+
+    /**
+     * This method returns true if array1 and array2 are equal
+     * if they are not equal it returns false
+     * arr1=[1,2,3,4] and arr2=[1,2,3,4] return true
+     * arr1=[1,2,3,4] and arr2=[5,6,7,8] return false
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public boolean isEqual(int[]arr1,int[]arr2)
+    {
+        return false;
+    }
+
+    /**
+     * This method returns the number of odd values inside the array
+     * arr=[1,2,3,2] returns 2, if the array is empty return 0
+     * @param arr
+     * @return
+     */
+    public int numberOfOddNumbers(int[] arr)
+    {
+        return 0;
+    }
+
+    /**
+     * This method returns the number of even values inside the array
+     * arr=[1,2,3,2] returns 2, if the array is empty return 0
+     * @param arr
+     * @return
+     */
+    public int numberOfEvenNumbers(int[] arr)
+    {
+        return 0;
+    }
+
+    /**
+     * This method returns the number of positive numbers in the array
+     * if the array is empty return zero
+     * arr=[1,2,-8,4] return 3
+     * @param arr
+     * @return
+     */
+    public int numberOfPositiveNumbers(int[] arr)
+    {
+        return 0;
+    }
+
+     /**
+     * This method returns the number of negative numbers in the array
+     * if the array is empty return zero
+     * arr=[1,2,-8,4] return 1
+     * @param arr
+     * @return
+     */
+    public int numberOfNegativeNumbers(int[] arr)
+    {
+        return 0;
+    }
+
+    /**
+     * This method returns the most occuring value in the array
+     * if the array is empty return zero
+     * arr=[1,3,4,6,1,7,1,9] return 1
+     * @param arr
+     * @return
+     */
+    public int mostOccuring(int[] arr)
+    {
+        return 0;
+    }
+
+    /**
+     * This method will sort the array by placing all 0's on the left of 
+     * the array and 1's on the right side of the array and return the 
+     * newly arranged array, arr=[1,0,1,0,1,0] returns [0,0,0,1,1,1]
+     * @param arr
+     * @return
+     */
+    public int[] sort(int[]arr)
+    {
+        return null;
+    }
+}
