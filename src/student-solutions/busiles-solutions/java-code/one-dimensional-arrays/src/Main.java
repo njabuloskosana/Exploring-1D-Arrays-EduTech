ffc75e7f1a401b2b0fb8f8a548ad2835c2389bc3
Njabulo <njabulo@xcontent.com>
2022-04-11 11:29:25 +0200

    clearing workspace errors

--- src/student-solutions/busiles-solutions/java-code/one-dimensional-arrays/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        
-        ArrayOperations operations = new ArrayOperations();
-        System.out.println("Add Your Code To Test The Operations");
-    }
-}
