7758519fdf78293564f036cb92fb885363f14a99
Njabulo <njabulo@xcontent.com>
2022-04-11 13:10:02 +0200

    completing code base setup

--- src/student-solutions/busiles-solutions/java-code/one-dimensional-arrays/src/Array.java
+++ src/student-solutions/busiles-solutions/java-code/one-dimensional-arrays/src/Array.java
@@ -1,7 +1,7 @@
 public class Array {
     public static void main(String[] args) throws Exception {
         
-        ArrayOperations operations = new ArrayOperations();
+       // ArrayOperations operations = new ArrayOperations();
         System.out.println("Add Your Code To Test The Operations");
     }
 }
